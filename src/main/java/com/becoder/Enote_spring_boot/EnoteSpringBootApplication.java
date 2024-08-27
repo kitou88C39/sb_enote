dcb60cd0197d61533eb20926a80015ad6c893e08
kitoumasaomi <kanfutrooper@gmail.com>
2024-08-27 10:13:13 +0900

    resouces/application.propertiesを編集する

---
+++ src/main/java/com/becoder/Enote_spring_boot/EnoteSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.becoder.Enote_spring_boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnoteSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnoteSpringBootApplication.class, args);
+	}
+
+}
